a84db598a86d3868d6099f26a138a9e68c56df6e
ahmet <ahmetersz@gmail.com>
2023-01-25 21:51:04 +0300

    ikinci  görevimi yaptım

--- src/Gun01/ogrenci.java
+++ src/Gun01/ogrenci.java
@@ -4,7 +4,8 @@ public class ogrenci {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("Merhaba");
-        }
 
+        }
+        System.out.println("merhaba 2");
     }
 }
