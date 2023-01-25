730c5adadfcaec458d238eea56b43c2e5e2781d6
ahmet <ahmetersz@gmail.com>
2023-01-25 21:49:51 +0300

    ilk görevimi yaptım

--- src/Gun01/ogrenci.java
+++ src/Gun01/ogrenci.java
@@ -2,7 +2,9 @@ package Gun01;
 
 public class ogrenci {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Merhaba");
+        }
 
     }
 }
